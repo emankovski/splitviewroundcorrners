1b95008f1cb4b04b3d9ec2bd89aade30076a3b78
Eugene Mankovski <emankovski@gmail.com>
2016-07-02 21:48:30 -0400

    Fixed

---
+++ Storyboards 1/Storyboards 1/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Storyboards 1
+//
+//  Created by Nik on 06/04/2016.
+//  Copyright © 2016 notro. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
